40bc396e6077d30ff441bc05fe62a6529451b979
Kalkidan Derso <dersokalkidan@gmail.com>
2022-05-25 23:25:38 +0300

    main container is divided

--- components/Contents/DashboardContent/index.js
+++ components/Contents/DashboardContent/index.js
@@ -11,6 +11,14 @@ export default function index() {
   return (
     
     <div className={classes.mainContent}>
+      <div className={classes.upperDivision}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci modi harum tenetur, corrupti veniam molestiae architecto inventore eos quod fugiat quisquam eveniet officia, quaerat illum qui necessitatibus ullam unde quia?
+        </p>
+      </div>
+      <div className={classes.lowerDivision}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellendus repellat dolorem, minima consequatur et sint nemo assumenda, earum id quae omnis inventore reprehenderit nam nisi rerum similique amet eum.
+      </div>
     </div>
   )
 }
