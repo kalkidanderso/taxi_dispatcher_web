0e69a18c796bc1ef74e67f8a8f261dd8b4f47fd7
Kalkidan Derso <dersokalkidan@gmail.com>
2022-05-28 16:09:14 +0300

    Modification

--- components/Orders/Pending.js
+++ components/Orders/Pending.js
@@ -4,7 +4,16 @@ import classes from "./order.module.css";
 export default function Pending(){
     return (
         <div className={classes.pending}>
-            <p>This is the pending functional components</p>
+            <div className={classes.pendingHeader}>
+                <ul className={classes.pendingItems}>
+                <li>Terminals</li>
+                <li>VIP Taxi</li>
+                <li>Mini Bus</li>
+                </ul>
+            </div>
+            <div className={classes.pendingBody}>
+                <p>This is the body</p>
+            </div>
         </div>
     );
 }
